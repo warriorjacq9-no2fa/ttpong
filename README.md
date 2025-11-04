a213cf781db4010f00fdb7cef58627108cd0ff2a
warriorjacq9 <jackflusche@gmail.com>
2025-11-03 18:53:32 -0800

    Tentative first push

---
+++ README.md
@@ -0,0 +1,6 @@
+![](../../workflows/gds/badge.svg) ![](../../workflows/test/badge.svg)
+
+# TinyTapeout Pong game
+This is a Pong game written entirely in Verilog-HDL for the TinyTapeout project. It supports 2 players and outputs in VGA format
+
+The external components needed are 3 sets of resistor ladders for analog colors, a VGA connector, and 3 buttons per player
---
+++ src/main.v
@@ -0,0 +1,15 @@
+module tt_um_pong (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+    assign uio_oe = 8'd0;
+    assign uio_out = 8'd0;
+    assign uo_out = ui_in + uio_in;
+    wire _unused = {ena, clk, rst_n, 0};
+endmodule
