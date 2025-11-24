2a8a0f59954545450fe9055619254fa132ea6dba
warriorjacq9 <jackflusche@gmail.com>
2025-11-24 15:22:51 -0800

    test: revert some changes to lower usage

--- src/sprite.v
+++ src/sprite.v
@@ -15,12 +15,9 @@ module sprite #( parameter
     localparam WIDTH_2 = WIDTH >> 1;
     localparam HEIGHT_2 = HEIGHT >> 1;
 
-    wire signed [9:0] dx_signed = $signed(x) - $signed(sx);
-    wire signed [9:0] dy_signed = $signed(y) - $signed({1'b0, sy});
-
     assign en = (
-        ($unsigned(dx_signed[8] ? -dx_signed : dx_signed) < WIDTH_2) &&
-        ($unsigned(dy_signed[9] ? -dy_signed : dy_signed) < HEIGHT_2)
+        (x > sx - WIDTH_2 && x < sx + WIDTH_2) &&
+        (y > sy - HEIGHT_2 && y < sy + HEIGHT_2)
     );
     assign r = {2{en}} & R;
     assign g = {2{en}} & G;
