941f701135038b5bfeea9ff137b7c9c1ebd23bfd
warriorjacq9 <jackflusche@gmail.com>
2025-11-24 15:15:51 -0800

    test: further optimizations for sprite logic

--- src/sprite.v
+++ src/sprite.v
@@ -12,11 +12,18 @@ module sprite #( parameter
     output wire [1:0] r, g, b,
     output wire en
 );
+    localparam WIDTH_2 = WIDTH >> 1;
+    localparam HEIGHT_2 = HEIGHT >> 1;
+
+    wire signed [9:0] dx_signed = $signed(x) - $signed(sx);
+    wire signed [9:0] dy_signed = $signed(y) - $signed({1'b0, sy});
 
     assign en = (
-        (x > sx - (WIDTH / 2) && x < sx + (WIDTH / 2)) &&
-        (y > sy - (HEIGHT / 2) && y < sy + (HEIGHT / 2))
+        ($unsigned(dx_signed[8] ? -dx_signed : dx_signed) < WIDTH_2) &&
+        ($unsigned(dy_signed[9] ? -dy_signed : dy_signed) < HEIGHT_2)
     );
-    assign {r, g, b} = en ? {R, G, B} : 6'bxxxxxx;
+    assign r = {2{en}} & R;
+    assign g = {2{en}} & G;
+    assign b = {2{en}} & B;
 
 endmodule
