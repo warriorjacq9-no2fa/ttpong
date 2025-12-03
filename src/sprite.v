f371a541a5265b363204789a1f6fea3e74f8eb9f
warriorjacq9-no2fa <jflusche3180@student.pps.net>
2025-12-03 19:22:11 +0000

    Minor/explicit optimizations

--- src/sprite.v
+++ src/sprite.v
@@ -3,15 +3,16 @@ module sprite #( parameter
     HEIGHT = 10
 )
 (
-    input wire [9:0] x, y,
+    input wire [9:0] x, 
+    input wire [9:0] y,
     input wire [9:0] sx,
     input wire [8:0] sy,
     output wire en
 );
 
-    assign en = (
-        (x > sx - (WIDTH / 2) && x < sx + (WIDTH / 2)) &&
-        (y > sy - (HEIGHT / 2) && y < sy + (HEIGHT / 2))
-    );
+    wire [9:0] dx = (x > sx) ? (x - sx) : (sx - x);
+    wire [9:0] dy = (y > {1'b0, sy}) ? (y - sy) : (sy - y);
+
+    assign en = (dx < (WIDTH/2)) && (dy < (HEIGHT/2));
 
 endmodule
